e8d864dd7b890ea0df21de760422cd5038aff4d9
Landen <ljbock68@gmail.com>
2024-10-08 10:25:01 -0500

    fixes

--- src/Components/Compare.js
+++ src/Components/Compare.js
@@ -20,7 +20,7 @@ const Compare = () => {
     const fetchMAL = async (username) => {
         try {
             let allAnime = [];
-            let nextPage = `https://tranquil-thicket-28555.herokuapp.com/https://api.myanimelist.net/v2/users/${username}/animelist?status=completed&fields=list_status`;
+            let nextPage = `https://cors-anywhere.herokuapp.com/https://api.myanimelist.net/v2/users/${username}/animelist?status=completed&fields=list_status`;
 
             while (nextPage) {
                 console.log('Fetching:', nextPage);
@@ -41,7 +41,7 @@ const Compare = () => {
                 console.log('Result:', result);
 
                 allAnime = [...allAnime, ...result.data];
-                nextPage = result.paging?.next ? `https://tranquil-thicket-28555.herokuapp.com/${result.paging.next}` : null;
+                nextPage = result.paging?.next ? `https://cors-anywhere.herokuapp.com/${result.paging.next}` : null;
 
                 console.log('Next Page:', nextPage);
             }
